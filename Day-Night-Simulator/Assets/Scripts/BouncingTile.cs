531ff765b604ef0ead0a526b3be07dc798209c51
fireflew11 <tombraudo2808@gmail.com>
2024-04-29 01:22:40 +0300

    works!!!

--- Day-Night-Simulator/Assets/Scripts/BouncingTile.cs
+++ Day-Night-Simulator/Assets/Scripts/BouncingTile.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -10,6 +11,12 @@ public class BouncingTile : MonoBehaviour
     public Tilemap removeFromTilemap;
     [SerializeField]
     public TileBase tileBase;
+    [SerializeField]
+    public float offset;
+    [SerializeField]
+    Vector3[] directions = null;
+    [SerializeField]
+    GameObject movingTilePrefab;
     private Rigidbody2D rb;
 
     Vector3 lastVelocity;
@@ -17,29 +24,69 @@ public class BouncingTile : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(new Vector2(15f, 15f));
-        // Set the initial velocity based on the specified direction and speed
-        //rb.velocity = initialDirection.normalized * speed;
+        rb.AddForce(new Vector2(25f, 25f));
     }
 
-    void FixedUpdate()
+    void Update()
     {
         lastVelocity = rb.velocity;
+        
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        var speed = lastVelocity.magnitude;
-        var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
-        rb.velocity = direction * speed;
 
-        foreach(var contact in collision.contacts)
+        for (int i = 0; i < collision.contacts.Length; i++)
         {
-            Vector3 worldPoint = contact.point;
-            Vector3Int gridPosition = addToTilemap.WorldToCell(worldPoint);
-            addToTilemap.SetTile(gridPosition, tileBase);
-            removeFromTilemap.SetTile(gridPosition, null);
+            //if (gameObject.name == "BouncingGrayTile")
+            //    Debug.Log(i);
+            if (i != 0 && (math.abs(collision.contacts[i - 1].point.x - collision.contacts[i].point.x) < 0.01f
+                       ||  math.abs(collision.contacts[i - 1].point.y - collision.contacts[i].point.y) < 0.01f))
+                continue;
+            var magni = rb.velocity.magnitude;
+            
+            var speed = lastVelocity.magnitude;
+            Vector2 v;
+            if (math.abs(collision.contacts[i].normal.x) > math.abs(collision.contacts[i].normal.y))
+                v = new Vector2(collision.contacts[i].normal.x, 0).normalized;
+            else
+                v = new Vector2(0, collision.contacts[i].normal.y).normalized;
+            var newDirection = Vector3.Reflect(lastVelocity.normalized, v);
+            if (gameObject.name == "BouncingGrayTile")
+                Debug.Log(lastVelocity.normalized + "   " + collision.contacts[i].normal + "    " + newDirection);
+            rb.velocity = newDirection * speed;
+            lastVelocity = rb.velocity;
         }
+        //rb.velocity = rb.velocity.normalized * ;
+       
+        var contact = collision.contacts[0];
+        Vector3 worldPoint = contact.point;
+        Vector3Int gridPosition = Vector3Int.zero;
+        foreach (var direction in directions)
+        {
+            gridPosition = removeFromTilemap.WorldToCell(worldPoint + direction.normalized * offset);
+            if (removeFromTilemap.GetTile(gridPosition) != null)
+                break;
+        }
+        addToTilemap.SetTile(gridPosition, tileBase);
+        removeFromTilemap.SetTile(gridPosition, null);
 
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (gameObject.name == "BouncingGrayTile")
+            Debug.Log(rb.velocity.magnitude);
+        var speed = lastVelocity.magnitude;
+        if (rb.velocity.magnitude < 7 )
+        {
+            Vector2 v;
+            if (math.abs(collision.contacts[0].normal.x) > math.abs(collision.contacts[0].normal.y))
+                v = new Vector2(collision.contacts[0].normal.x, 0).normalized;
+            else
+                v = new Vector2(0, collision.contacts[0].normal.y).normalized;
+            var newDirection = Vector3.Reflect(lastVelocity.normalized, v);
+            rb.velocity = newDirection * speed;
+            lastVelocity = rb.velocity;
+        }
     }
 }
